5775fd5cb6b17009cb1476e919c2b5820ff80c1c
Brokkoliverschwendung <Oliver.Schulze00@gmail.com>
2023-03-21 21:12:16 +0100

    LLO-15
    git commit -m "LLO-15 <message>"
    Barrieren + ScoreBoard

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:test_application/barriers.dart';
 import 'package:test_application/bird.dart';
+
+import 'barriers.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -16,6 +19,9 @@ class _HomePageState extends State<HomePage> {
   double height = 0;
   double initialHeight = birdYaxis;
   bool gameHasStarted = false;
+  static double barrierXone = 1;
+  double barrierXtwo = barrierXone + 1.5;
+
 
   void jump() {
     setState(() {
@@ -33,6 +39,22 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         birdYaxis = initialHeight - height;
       });
+      setState(() {
+        if (barrierXone < -1.6) {
+          barrierXone += 3.1;
+        } else {
+          barrierXone -= 0.05;
+        }
+      });
+
+      setState(() {
+        if (barrierXtwo < -1.6) {
+          barrierXtwo += 3.1;
+        } else {
+          barrierXtwo -= 0.05;
+        }
+      });
+
       if (birdYaxis > 1) {
         timer.cancel();
         gameHasStarted = false;
@@ -42,57 +64,99 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(flex: 2,
-            child: GestureDetector(
-              onTap: () {
-                if (gameHasStarted) {
-                  jump();
-                } else {
-                  startGame();
-                }
-              },
-
-              child: AnimatedContainer(
-                alignment: Alignment(0,birdYaxis),
-                duration: Duration(milliseconds: 0),
-                color: Colors.blue,
-                child: MyBird(),
-              ),
-            ),
-          ),
-          Container(
-            height: 15,
-            color: Colors.green,
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.brown,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return GestureDetector(
+      onTap: () {
+        if (gameHasStarted) {
+          jump();
+        } else {
+          startGame();
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(flex: 2,
+              child: Stack(
                 children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Score", style: TextStyle(color: Colors.black, fontSize: 20)),
-                    SizedBox(height: 20,),
-                    Text("0", style: TextStyle(color: Colors.black, fontSize: 35)),
-                  ],
+                  AnimatedContainer(
+                    alignment: Alignment(0,birdYaxis),
+                    duration: Duration(milliseconds: 0),
+                    color: Colors.blue,
+                    child: MyBird(),
+                  ),
+                  Container(
+                    alignment: Alignment(0, -0.25),
+                    child: gameHasStarted
+                        ? Text(" ")
+                        : Text("T A P  T O  P L A Y",
+                          style: TextStyle(
+                            fontSize: 21,
+                            color: Colors.white
+                        )),
+                  ),
+                AnimatedContainer(
+                  alignment: Alignment(barrierXone, 1.1),
+                  duration: Duration(milliseconds: 0),
+                  child: MyBarrier(
+                    size: 200.0,
+                  ),
                 ),
+                AnimatedContainer(
+                  alignment: Alignment(barrierXone, -1.1),
+                  duration: Duration(milliseconds: 0),
+                  child: MyBarrier(
+                    size: 200.0,
+                  ),
+                ),
+                AnimatedContainer(
+                  alignment: Alignment(barrierXtwo, 1.1),
+                  duration: Duration(milliseconds: 0),
+                  child: MyBarrier(
+                    size: 150.0,
+                  ),
+                ),
+                AnimatedContainer(
+                  alignment: Alignment(barrierXtwo, -1.1),
+                  duration: Duration(milliseconds: 0),
+                  child: MyBarrier(
+                    size: 250.0,
+                  ),
+                )
+
+                ],
+              )
+            ),
+            Container(
+              height: 15,
+              color: Colors.green,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.brown,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Highscore", style: TextStyle(color: Colors.black, fontSize: 20)),
+                      Text("Score", style: TextStyle(color: Colors.black, fontSize: 20)),
                       SizedBox(height: 20,),
-                      Text("10", style: TextStyle(color: Colors.black, fontSize: 35)),
+                      Text("0", style: TextStyle(color: Colors.black, fontSize: 35)),
                     ],
                   ),
-              ],),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Highscore", style: TextStyle(color: Colors.black, fontSize: 20)),
+                        SizedBox(height: 20,),
+                        Text("10", style: TextStyle(color: Colors.black, fontSize: 35)),
+                      ],
+                    ),
+                ],),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
